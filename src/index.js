8ce1654cf60153c5e001563a9aa32d475f035917
radville <laura.radville@gmail.com>
2019-10-18 10:23:15 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+
+  const taskForm = document.getElementById("create-task-form");
+
+  // won't redirect when 'submit' is clicked
+  taskForm.addEventListener('click', function(event) {
+    event.preventDefault();
+    let taskDescription = document.getElementById("new-task-description").value;
+
+    // make a new task
+    insertTask(taskDescription)
+  })
 });
+
+
+function insertTask(task) {
+  const node = document.createElement('li');
+  const textNode = document.createTextNode(task);
+  node.appendChild(textNode);
+  document.getElementById("main-content").appendChild(node)
+}
